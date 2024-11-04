7a12cd1ba2d298d7c2579bfa3bb62cff83b90a51
Gopal3746 <72813883+Gopal3746@users.noreply.github.com>
2024-11-03 21:27:13 -0500

    Update BladeScript.cs

--- Assets/BladeScript.cs
+++ Assets/BladeScript.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 
 public class BladeScript : MonoBehaviour
 {
-    public GameObject blade; // Reference to the first blade GameObject
-    public GameObject blade2; // Reference to the second blade GameObject (for double blade)
+    public GameObject blade;
+    public GameObject blade2;
     public float extendSpeed = 0.1f;
 
     [Header("Audio Settings")]
-    public AudioSource audioSource;  // Reference to AudioSource component
-    public AudioClip powerOnSound;   // Power on sound effect
-    public AudioClip powerOffSound;  // Power off sound effect
-    public AudioClip themeMusic;      // Darth Vader theme music
+    public AudioSource audioSource;
+    public AudioClip powerOnSound;
+    public AudioClip powerOffSound;
+    public AudioClip themeMusic;
 
     private bool weaponActive = true;
-    private bool isDoubleBladed = false; // Track whether it's double-bladed
+    private bool isDoubleBladed = false;
     private float scaleMin = 0f;
     private float scaleMax;
     private float extendDelta;
@@ -32,7 +32,6 @@ public class BladeScript : MonoBehaviour
         extendDelta = scaleMax / extendSpeed;
         weaponActive = true;
 
-        // Initially set the second blade to inactive
         blade2.SetActive(false);
     }
 
@@ -55,21 +54,17 @@ public class BladeScript : MonoBehaviour
                 audioSource.PlayOneShot(powerOnSound);
             }
 
-            // Reverse extendDelta to toggle extend/retract
             extendDelta = weaponActive ? -Mathf.Abs(extendDelta) : Mathf.Abs(extendDelta);
         }
 
-        // Update blade extension/retraction
         scaleCurrent += extendDelta * Time.deltaTime;
         scaleCurrent = Mathf.Clamp(scaleCurrent, scaleMin, scaleMax);
         blade.transform.localScale = new Vector3(localScaleX, scaleCurrent, localScaleZ);
         blade.transform.localPosition = new Vector3(0, scaleCurrent / 2, 0);
 
-        // Set active state of the blade
         weaponActive = scaleCurrent > 0;
         blade.SetActive(weaponActive);
 
-        // If the blade is off, hide the second blade too
         if (!weaponActive)
         {
             blade2.SetActive(false);
@@ -80,24 +75,21 @@ public class BladeScript : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.K))
         {
-            isDoubleBladed = !isDoubleBladed; // Toggle double blade state
-            blade2.SetActive(isDoubleBladed && weaponActive); // Show or hide the second blade based on state
+            isDoubleBladed = !isDoubleBladed;
+            blade2.SetActive(isDoubleBladed && weaponActive);
 
             if (isDoubleBladed)
             {
-                // Position the second blade at the opposite end of the hilt
                 blade2.transform.localPosition = new Vector3(0, -1.5f / 2, 0);
                 blade2.transform.localScale = new Vector3(localScaleX, scaleCurrent, localScaleZ);
 
-                // Play the theme music
                 audioSource.clip = themeMusic;
-                audioSource.loop = true; // Set the audio to loop
-                audioSource.Play(); // Start playing the theme music
+                audioSource.loop = true;
+                audioSource.Play();
             }
             else
             {
-                // Stop the theme music
-                audioSource.Stop(); // Stop playing the theme music
+                audioSource.Stop();
             }
         }
     }
